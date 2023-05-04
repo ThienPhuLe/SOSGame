bd68b638140379d539438a499a8f23208d4df878
Austin Le <Austin Le@DESKTOP-KSED0BO>
2023-05-04 15:45:01 -0500

    S.O.S Game

--- src/aiModeClass.java
+++ src/aiModeClass.java
@@ -1,8 +1,6 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 public class aiModeClass {
     private MyFrame myFrame;
     private checkMovePerfromed myMove;
@@ -11,10 +9,18 @@ public class aiModeClass {
     int sizeS = 0;
     int aiRowO, aiRowS;
     int aiColO, aiColS;
+    Random rand = new Random();
+    List<Pair> emptyMove = new ArrayList<Pair>();
 
     public aiModeClass(MyFrame myFrame, checkMovePerfromed myMove) {
         this.myFrame = myFrame;
         this.myMove = myMove;
+    }
+
+    public Pair randomPair (List<Pair> emptyMove)
+    {
+        int randPair = rand.nextInt(emptyMove.size() - 1);
+        return emptyMove.get(randPair);
     }
 
 
@@ -22,6 +28,7 @@ public class aiModeClass {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (myFrame.sosButtons[i][j].getText() == "") {
+                    emptyMove.add(new Pair(i,j));
                     if (sizeO < myMove.winCondition(i, j, "O").size()) {
                         sizeO = myMove.winCondition(i, j, "O").size();
                         aiRowO = i;
@@ -50,80 +57,48 @@ public class aiModeClass {
                 myFrame.player2ButtonS.doClick();
                 myFrame.sosButtons[aiRowS][aiColS].doClick();
             } else if (sizeO == sizeS && sizeO == 0 && sizeS == 0) {
-                for (int i = 0; i < row; i++) {
-                    for (int j = 0; j < col; j++) {
-                        if (myFrame.sosButtons[i][j].getText() == "") {
-                            if (myFrame.player2Text.equals("S")) {
-                                myFrame.player2ButtonO.doClick();
-                                myFrame.sosButtons[i][j].doClick();
-                                check = true;
-                            } else if (myFrame.player2Text.equals("O")) {
-                                myFrame.player2ButtonS.doClick();
-                                myFrame.sosButtons[i][j].doClick();
-                                check = true;
-                            }
-
-                            if (check == true) {
-                                break;
-                            }
-
-                        }
-                    }
-
-                    if (check == true) {
-                        break;
-                    }
+                Pair nextMovePair = randomPair(emptyMove);
+                if (myFrame.player2Text.equals("S")) {
+                    myFrame.player2ButtonO.doClick();
+                    myFrame.sosButtons[nextMovePair.getR()][nextMovePair.getC()].doClick();
+                    check = true;
+                } else if (myFrame.player2Text.equals("O")) {
+                    myFrame.player2ButtonS.doClick();
+                    myFrame.sosButtons[nextMovePair.getR()][nextMovePair.getC()].doClick();
+                    check = true;
                 }
             }
         }
-
         else if (player == "Player 1 Turn") {
-                if (sizeO > sizeS) {
+            if (sizeO > sizeS) {
+                myFrame.player1ButtonO.doClick();
+                myFrame.sosButtons[aiRowO][aiColO].doClick();
+                sizeO = 0;
+                sizeS = 0;
+            } else if (sizeO < sizeS) {
+                myFrame.player1ButtonS.doClick();
+                myFrame.sosButtons[aiRowS][aiColS].doClick();
+                sizeO = 0;
+                sizeS = 0;
+            } else if (sizeO == sizeS && sizeO > 0) {
+                myFrame.player1ButtonS.doClick();
+                myFrame.sosButtons[aiRowS][aiColS].doClick();
+                sizeO = 0;
+                sizeS = 0;
+            } else if (sizeO == sizeS && sizeO == 0 && sizeS == 0) {
+                Pair nextMovePair = randomPair(emptyMove);
+                if (myFrame.player1Text.equals("S")) {
                     myFrame.player1ButtonO.doClick();
-                    myFrame.sosButtons[aiRowO][aiColO].doClick();
-                    sizeO =0;
-                    sizeS =0;
-                } else if (sizeO < sizeS) {
+                    myFrame.sosButtons[nextMovePair.getR()][nextMovePair.getC()].doClick();
+                    check = true;
+                } else if (myFrame.player1Text.equals("O")) {
                     myFrame.player1ButtonS.doClick();
-                    myFrame.sosButtons[aiRowS][aiColS].doClick();
-                    sizeO =0;
-                    sizeS =0;
-                } else if (sizeO == sizeS && sizeO > 0) {
-                    myFrame.player1ButtonS.doClick();
-                    myFrame.sosButtons[aiRowS][aiColS].doClick();
-                    sizeO =0;
-                    sizeS =0;
-                } else if (sizeO == sizeS && sizeO == 0 && sizeS == 0) {
-                    for (int i = 0; i < row; i++) {
-                        for (int j = 0; j < col; j++) {
-                            if (myFrame.sosButtons[i][j].getText() == "") {
-                                if (myFrame.player1Text.equals("S")) {
-                                    myFrame.player1ButtonO.doClick();
-                                    myFrame.sosButtons[i][j].doClick();
-                                    check = true;
-                                } else if (myFrame.player1Text.equals("O")) {
-                                    myFrame.player1ButtonS.doClick();
-                                    myFrame.sosButtons[i][j].doClick();
-                                    check = true;
-                                }
-
-                                if (check == true) {
-                                    break;
-                                }
-
-                            }
-                        }
-
-                        if (check == true) {
-                            sizeO =0;
-                            sizeS =0;
-                            break;
-                        }
-                    }
+                    myFrame.sosButtons[nextMovePair.getR()][nextMovePair.getC()].doClick();
+                    check = true;
                 }
-
-
             }
-        }
-}
 
+
+        }
+    }
+}
